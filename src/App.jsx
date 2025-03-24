18bcfbd92000da2efcdce3b9c2ea192143b090b0
sitabasnet <sitabasnet095@gmail.com>
2025-03-24 13:43:07 +1100

    forget password feature

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button>
+          <H2>ForgetPassword</H2>
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
