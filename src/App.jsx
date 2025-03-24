75728f82283ee6246cd21d9964b93fb3e11871df
Ashon Shakya <shakya.ashon@gmail.com>
2025-03-24 13:44:28 +1100

    update feature

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           count is {count}
         </button>
         <button>FEATURE 1</button>
+        <button>FEATURE 1</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
